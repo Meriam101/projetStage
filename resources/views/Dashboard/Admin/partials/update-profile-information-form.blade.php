b935e85bf7762ea227874694e21fc866459e3052
09262005 <ayacheomaima19@gmail.com>
2025-04-28 14:11:24 +0200

    first

--- resources/views/Dashboard/Admin/partials/update-profile-information-form.blade.php
+++ resources/views/Dashboard/Admin/partials/update-profile-information-form.blade.php
@@ -3,7 +3,6 @@
         <h2 class="text-lg font-medium text-gray-900">
             {{ __('Profile Information') }}
         </h2>
-
         <p class="mt-1 text-sm text-gray-600">
             {{ __("Update your account's profile information and email address.") }}
         </p>
@@ -12,7 +11,6 @@
     <form id="send-verification" method="post" action="{{ route('verification.send') }}">
         @csrf
     </form>
-
     <form method="post" action="{{ route('admin.profile.update') }}" class="mt-6 space-y-6">
         @csrf
         @method('patch')
